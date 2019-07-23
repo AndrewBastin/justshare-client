a68d51b0143b7c57b9295936d076bb5f269803e1
Andrew Bastin <andrewbastin.k@gmail.com>
2019-07-23 13:43:56 +0530

    Bump version to alpha-13

--- src/App.tsx
+++ src/App.tsx
@@ -248,7 +248,7 @@ export default class App extends React.Component<{}, State> {
     public render(): JSX.Element {
         return (
             <div className="App">
-                <h3 style={{ textAlign: 'center' }}>JustShare [Alpha 12]</h3>
+                <h3 style={{ textAlign: 'center' }}>JustShare [Alpha 13]</h3>
                 <p style={{ textAlign: 'center' }}>You are : {this.state.nickname ? `${this.state.nickname} (id : ${this.state.socketID})` : this.state.socketID}</p>
                 { this.renderCurrentPage() }
             </div>
