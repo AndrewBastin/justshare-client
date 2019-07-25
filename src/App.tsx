8e6fae8ddbc4b70bf763ba81fed3d2888bea133f
Andrew Bastin <andrewbastin.k@gmail.com>
2019-07-25 22:50:12 +0530

    Bump version to alpha-17

--- src/App.tsx
+++ src/App.tsx
@@ -262,7 +262,7 @@ export default class App extends React.Component<{}, State> {
     public render(): JSX.Element {
         return (
             <div className="App">
-                <h3 style={{ textAlign: 'center' }}>JustShare [Alpha 16]</h3>
+                <h3 style={{ textAlign: 'center' }}>JustShare [Alpha 17]</h3>
                 <p style={{ textAlign: 'center' }}>
                     {
                         window.localStorage.getItem("nickname") ?
